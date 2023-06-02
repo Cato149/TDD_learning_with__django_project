8729f5469b88445df0eba880b83220183df37bdd
Cato <nick.tuchka@gmail.com>
2023-06-02 18:29:42 +0300

    now FT checks if we can add element in our list

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,7 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,29 +18,43 @@ class NewVisorTest(unittest.TestCase):
     
     def test_can_start_a_list_and_retrever_it_later(self):
         '''тест: тможно начать список и получить его позже'''
-        # * Мэту посоветоали новое крутое приложение для ведни списка дел
+        #Мэту посоветоали новое крутое приложение для ведни списка дел
         self.browser.get('http://localhost:8000')
         
-        # * Он видит, что заголовок и шапка говорят о списке дел (to-do)
+        #Он видит, что заголовок и шапка говорят о списке дел (to-do)
         self.assertIn('To-Do list', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
         
-        # * Он видит поле ввода новой задачи
+        #Он видит поле ввода новой задачи
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
+        #Решает попробовать созать тебе дело на завтра. вводит: "Купить по пути домой молоко"
+        inputbox.send_keys('Купить молоко')
 
-        # * Решает попробовать созать тебе дело на завтра. вводит: "Купить по пути домой молоко"
+        # Нажав сохранить страница перезагружается и теперь Мэт видит поле ввода и задачу, которую только что добавил
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить молоко' for row in rows)
+        )
+        # Он добавляет еще пару задач
+        self.fail('Закончить тест!')
+        # Страница обновившись показыват каждый новый элемент
 
-        # * Нажав сохранить страница перезагружается и теперь Мэт видит поле ввода и задачу, которую только что добавил
+        # Мэт решает сохранить свой список. Он нажимает на кнопку и страница генерирует для него 
+        # URL по которому он может получить доступ к своему списку откуда удобно
 
-        # * Он добавляет еще пару задач
+        # Он переходит по ссылке и видит свой список 
 
-        # * Страница обновившись показыват каждый новый элемент
-
-        # * Мэт решает сохранить свой список. Он нажимает на кнопку и страница генерирует для него 
-        # * URL по которому он может получить доступ к своему списку откуда удобно
-
-        # * Он переходит по ссылке и видит свой список 
-
-        # * Наконец сайт выдыхает, ведь этот душнила ушел
+        # Наконец сайт выдыхает, ведь этот душнила ушел
 
         
 if __name__ == '__main__':
