8b6c3f596032ee31cbbc7325ce378e3a0faf61d0
Cato <nick.tuchka@gmail.com>
2023-06-11 21:56:58 +0300

    updated model Item and added new model List

--- superlist/lists/models.py
+++ superlist/lists/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class List(models.Model):
+    '''Это список'''
+    pass
+
 class Item(models.Model):
     '''Эллемент списка'''
     text = models.TextField(default="")
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
